53191be7ed75d0aa8637fe12453c4bfecff099aa
Pavel Pullso <p.dubitskiy@ya.ru>
2020-02-24 22:28:11 +0500

    add section pricing

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  /* Портфолио */
+  $('.button[filter]').click(function() {
+    if ($(this).attr('val') == 'off') {
+      $('.button[filter]')
+        .attr('val', 'off')
+        .removeClass('focused')
+      $(this)
+        .attr('val', 'on')
+        .addClass('focused')
+      $('.filter > div').hide(300)
+      $('.filter > div[filter=' + $(this).attr('filter') + ']').show(300)
+      if ($(this).attr('filter') == 'all') {
+        $('.button[filter]')
+          .attr('val', 'off')
+          .removeClass('focused')
+        $(this)
+          .attr('val', 'on')
+          .addClass('focused')
+        $('.filter > div').show(300)
+      }
+    }
+  })
+})
